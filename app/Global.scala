07d810296ea0d29aeb54daa52a2ef08492592ddd
Hadi Ahmadi <MacPersia@gmail.com>
2016-02-14 12:54:17 +0800

    Improved logging as well as visual representation of discrepancies.

---
+++ app/Global.scala
@@ -0,0 +1,33 @@
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import play.api.Logger
+import play.api.mvc._
+import play.api._
+
+object Global extends WithFilters(AccessLoggingFilter) {
+
+  override def onStart(app: Application) {
+    Logger.info("Application has started")
+  }
+
+  override def onStop(app: Application) {
+    Logger.info("Application has stopped")
+  }
+}
+
+object AccessLoggingFilter extends Filter {
+
+  val accessLogger = Logger("access")
+
+  def apply(next: (RequestHeader) => Future[Result])(request: RequestHeader): Future[Result] = {
+    val resultFuture = next(request)
+
+    resultFuture.foreach(result => {
+      val msg = s"method=${request.method} uri=${request.uri} remote-address=${request.remoteAddress}" +
+        s" status=${result.header.status}";
+      accessLogger.info(msg)
+    })
+
+    resultFuture
+  }
+}
